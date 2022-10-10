9ac97c59568f18a1495ca2eb254e9a695f3374aa
Godi Blasio <100287431+BlasioGodi@users.noreply.github.com>
2022-10-10 15:51:39 +0300

    String Manipulation - Original

---
+++ String Manipulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greetings = "Hello World!";
+            string first_name = "Geoffrey";
+            string second_name = "Julie";
+            string third_name = "Carlson";
+            string error_message = "The exception was handled accordingly";
+            try
+            {
+                Console.WriteLine($"The original title for the greeting is {greetings}, which is common in most programming languages.");
+                Console.WriteLine($"The name for the first student is {first_name}.");
+                Console.WriteLine($"The name for the second student is {second_name}.");
+                Console.WriteLine($"The name for the third student is {third_name}.");
+
+            }
+            catch (Exception e)
+            {
+                error_message = e.Message;
+            }
+        }
+    }
+}
