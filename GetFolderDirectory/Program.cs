523308c581a9d52e61f324b37151680a60d8e992
Godi Blasio <100287431+BlasioGodi@users.noreply.github.com>
2022-10-20 21:23:36 +0300

    Folder File Path

---
+++ GetFolderDirectory/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FolderDir
+{
+    public class GetFolderDirectory
+    {
+        public static void Main()
+        {
+            Console.WriteLine("The folder directory is as below: ");
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData));
+
+        }
+    }
+}
